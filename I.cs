f427b383516f22ea8e078acb756b7479575df85f
Froppy06 <116511725+Froppy06@users.noreply.github.com>
2023-05-09 21:39:31 +0200

    Initial commit

---
+++ I.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A - Pole sześciana, B - Objętość koła, C - Objętość walca");
+            Console.WriteLine("Prosze podaj co chcesz obliczyć");
+            string inp = Console.ReadLine().ToUpper();
+            double x = double.Parse(Console.ReadLine());
+            double r = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            switch (inp)
+            {
+                case "A":
+                    Console.WriteLine(6*(x*x));
+                    break;
+                case "B":
+                    Console.WriteLine(4/3*x*(r*r*r));
+                    break;
+                case "C":
+                    Console.WriteLine(x*(r*r)*h);
+                    break;
+                default:
+                    Console.WriteLine("Nie ma takiej metody");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+       
---
+++ III.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A - Pole kuli, B - Objętość kuli, C - X, E - EXIT");
+            Console.WriteLine("Prosze podaj co chcesz obliczyć");
+            string inp = Console.ReadLine().ToUpper();
+            double x = double.Parse(Console.ReadLine());
+            double r = double.Parse(Console.ReadLine());
+            switch (inp)
+            {
+                case "A":
+                    Console.WriteLine(4 / 3 * x * (r * r * r));
+                    break;
+                case "B":
+                    Console.WriteLine(4 * x * (r*r));
+                    break;
+                case "C":
+                    Console.WriteLine();
+                    break;
+                case "E":
+                    Console.WriteLine("EXIT");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+       
